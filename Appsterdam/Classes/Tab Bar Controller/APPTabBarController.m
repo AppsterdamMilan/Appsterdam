31b596ad28c687602693ab4904318152f661faf0
Mouhcine <mouhcine.elamine@icloud.com>
2013-11-14 10:36:18 +0100

    Added APPTabBarController

---
+++ Appsterdam/Classes/Tab Bar Controller/APPTabBarController.m
@@ -0,0 +1,23 @@
+//
+//  APPTabBarController.m
+//  Appsterdam
+//
+//  Created by Mouhcine El Amine on 14/11/13.
+//  Copyright (c) 2013 Alessio Roberto. All rights reserved.
+//
+
+#import "APPTabBarController.h"
+#import "APPMeetupOperationManager.h"
+
+@implementation APPTabBarController
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    static BOOL didLogin = NO;
+    [APPMeetupOperationManager authorizeWithCompletion:^(BOOL success, NSError *error) {
+        didLogin = success;
+    }];
+}
+
+@end
