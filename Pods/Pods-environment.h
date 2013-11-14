4ebc581d3ffac3b91563954d1a45b26c9c0b5f73
Mouhcine <mouhcine.elamine@icloud.com>
2013-11-14 01:31:08 +0100

    Removed pods from .gitignore
    This way there's no need to run "pod install" to be able to run the
    project

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 3
+
+// AFOAuth2Client
+#define COCOAPODS_POD_AVAILABLE_AFOAuth2Client
+#define COCOAPODS_VERSION_MAJOR_AFOAuth2Client 0
+#define COCOAPODS_VERSION_MINOR_AFOAuth2Client 1
+#define COCOAPODS_VERSION_PATCH_AFOAuth2Client 2
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 8
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
+
+// SVProgressHUD
+#define COCOAPODS_POD_AVAILABLE_SVProgressHUD
+#define COCOAPODS_VERSION_MAJOR_SVProgressHUD 1
+#define COCOAPODS_VERSION_MINOR_SVProgressHUD 0
+#define COCOAPODS_VERSION_PATCH_SVProgressHUD 0
+
