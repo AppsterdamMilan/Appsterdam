0ae1b680c2f82692e566ff1aaf0b371471365df9
Mouhcine <mouhcine.elamine@icloud.com>
2013-11-14 03:03:37 +0100

    Added APPEvent and defined its interface.

---
+++ Appsterdam/Classes/Model/APPEvent.m
@@ -0,0 +1,22 @@
+//
+//  APPEvent.m
+//  Appsterdam
+//
+//  Created by Mouhcine El Amine on 14/11/13.
+//  Copyright (c) 2013 Alessio Roberto. All rights reserved.
+//
+
+#import "APPEvent.h"
+
+@implementation APPEvent
+
+-(instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
