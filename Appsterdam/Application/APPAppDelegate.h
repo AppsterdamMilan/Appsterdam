949282b061f00dbf8f60775e406a21d39ad6d840
Klaas Speller <klaasspeller@gmail.com>
2013-11-17 14:09:15 +0200

    Move all controller into correct folder and begin style guide

--- Appsterdam/Application/APPAppDelegate.h
+++ Appsterdam/Application/APPAppDelegate.h
@@ -1,12 +1,3 @@
-//
-//  APPAppDelegate.h
-//  Appsterdam
-//
-//  Created by Alessio Roberto on 17/10/13.
-//  Copyright (c) 2013 Alessio Roberto. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
 
 @interface APPAppDelegate : UIResponder <UIApplicationDelegate>
 
